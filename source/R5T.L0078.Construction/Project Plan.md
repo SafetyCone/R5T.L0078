d26321cc734d84fdec034f009388e91581a5bd6b
davidcoats <david@getsafetycone.com>
2024-01-14 17:22:48 -0800

    Initial commit.

---
+++ source/R5T.L0078.Construction/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.L0078.Construction
+Construction console project for the R5T.L0078 library.
---
+++ source/R5T.L0078/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.L0078
+{
+	/// <summary>
+	/// Octokit-based strictly-framework functionality library. (netstandard2.1)
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
