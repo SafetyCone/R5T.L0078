45b03eb1575ba55244e1891f8cbad037a88bad01
davidcoats <david@getsafetycone.com>
2024-06-04 12:45:25 -0700

    Interim commit.

--- source/R5T.L0078.F001/Code/Values/IValues.cs
+++ source/R5T.L0078.F001/Code/Values/IValues.cs
@@ -8,6 +8,9 @@ namespace R5T.L0078.F001
     [ValuesMarker]
     public partial interface IValues : IValuesMarker
     {
+        /// <summary>
+        /// <para><value>R5T.L0078.F001-Default</value></para>
+        /// </summary>
         public string DefaultProductHeaderValue => "R5T.L0078.F001-Default";
     }
 }
